55a39c9c7ac1dca8117ae8fb322702bd54496a46
Seto <sogotsyan.ssl@yandex.ru>
2026-02-03 21:52:50 +0300

    first commit

---
+++ hw1/ex1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Let's\n");
+    printf("  go\n");
+    printf("   to walk\n");
+    return 0;
+}
---
+++ hw1/ex2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HHOHH\n");
+    printf(" ZZZZZ\n");
+    
+    return 0;
+}
